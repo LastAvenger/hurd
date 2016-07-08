5c2a6bb39d243b4412ac8735d639160d7d176e13
Shengyu Zhang <lastavengers@outlook.com>
2016-07-08 16:06:46 +0800

    Stub code ;)

---
+++ libdiskfs/file-set-xattr.c
@@ -0,0 +1,42 @@
+/* libdiskfs implementation of fs.defs: file_set_xattr
+   Copyright (C) 2016 Free Software Foundation, Inc.
+
+   Written by Shengyu Zhang <lastavengers@outlook.com>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
+
+#include "fs_S.h"
+
+/* Implement file_set_xattr as described in <hurd/fs.defs>. */
+kern_return_t
+diskfs_S_file_set_xattr (struct protid *cred,
+			 char *name,
+			 char *value,
+			 size_t len,
+			 int flag)
+{
+  struct node *np;
+  error_t err = 0;
+
+  if (!cred)
+    return EOPNOTSUPP;
+
+  np = cred->po->np;
+
+  pthread_mutex_lock (&np->lock);
+  pthread_mutex_unlock (&np->lock);
+
+  return err;
+}
